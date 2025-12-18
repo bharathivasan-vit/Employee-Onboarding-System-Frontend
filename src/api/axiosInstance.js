c19558739165ffd88a95c62112724dfc67c1c089
bharathivasan-vit <bharathivasan.r@vit.ac.in>
2025-12-18 09:44:18 +0530

    after build

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { generateJwtToken } from "./generateJwtToken";
 
 const axiosInstance = axios.create({
-    baseURL : "http://172.16.1.107:8010/employee-onboarding-system/eobs",
+    baseURL : "http://localhost:8010/employee-onboarding-system/eobs",
     headers :  {
         'Content-Type': 'application/json',
     }
